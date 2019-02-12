3d365b2bcc3a4975c50fe4a4516912640859e812
mrachana <47312480+mrachana@users.noreply.github.com>
2019-02-12 08:55:52 +0530

    Create CheckSign.java

---
+++ CheckSign.java
@@ -0,0 +1,15 @@
+class CheckSign{
+public static void main(String[] qwe){
+int a=10;
+
+if (a<0){
+System.out.println("negative");
+}
+else if(a==0){
+System.out.println("zero");
+}
+else{
+System.out.println("positive");
+}
+}
+}

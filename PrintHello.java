6d360b55da600e72fc9dd26fe35feb4bfaedf4ce
mrachana <47312480+mrachana@users.noreply.github.com>
2019-02-12 11:31:19 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class PrintHello{
+public static void main(String[]qwe){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the value of N: ");
+int N=scan.nextInt();
+int X=1;
+while(X<=N){
+System.out.println("Hello");
+X++;
+}
+}
+}

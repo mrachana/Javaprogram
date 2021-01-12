5c6f98667240a051fe1f2ac4d46991d8b00d12b7
mrachana <47312480+mrachana@users.noreply.github.com>
2021-01-12 20:21:36 +0530

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class prg3
+{
+public static void main(String arg[])
+{
+ boolean a=10>20;
+System.out.println(a);
+}
+}
---
+++ string.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class string
+{
+public static void main(String arg[])
+{
+System.out.print("Welcome to java");
+}
+}
